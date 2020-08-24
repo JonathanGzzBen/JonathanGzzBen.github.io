79d85cd1a39fec2c306375790b5822352e2d0e3c
JonathanGzzBen <jonathangzzben@gmail.com>
2020-08-24 13:12:49 -0500

    Modify navbar to stick to top

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Navbar, Nav } from "react-bootstrap";
 
 const NavBar = (props) => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="sticky-top">
       <Navbar.Brand>JonathanGzzBen</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
