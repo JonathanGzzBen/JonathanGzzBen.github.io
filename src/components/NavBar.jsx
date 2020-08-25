320ffe4284167ea5de3a50cf2b7aaec1f1896395
JonathanGzzBen <jonathangzzben@gmail.com>
2020-08-25 11:05:26 -0500

    Change navbar brand

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Navbar, Nav } from "react-bootstrap";
 const NavBar = (props) => {
   return (
     <Navbar bg="primary" variant="dark" expand="lg" className="sticky-top">
-      <Navbar.Brand>JonathanGzzBen</Navbar.Brand>
+      <Navbar.Brand>JonathanGzz</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
